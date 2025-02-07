a0827df4e5d1e29d870574cd28abc2e387ecb39e
sawne <gml015@latech.edu>
2025-02-07 14:31:13 -0600

    added testing messages for parent and child processes

--- lab4/question1.c
+++ lab4/question1.c
@@ -3,6 +3,14 @@
 
 int main() {
   pid_t pid = fork();
+  if (pid < 0) {
+    printf("fork failed\n");
+  }
+  if (pid == 0) {
+    printf("I am the child process\n");
+  } else {
+    printf("I am the parent process\n");
+  }
 
   return 0;
 }
