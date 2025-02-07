a61f4edf0d5bc624dc07176dff57efa6b10e1592
sawne <gml015@latech.edu>
2025-02-07 14:48:51 -0600

    implemented solution to the problem

--- lab4/question1.c
+++ lab4/question1.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <wait.h>
 
-int main() {
+int main(int argc, char *argv[] {
+  if (argc < 2) {
+    printf("Usage: %s file\n", argv[0]);
+    return 1;
+  }
+
   pid_t pid = fork();
+
   if (pid < 0) {
     printf("fork failed\n");
+    return 1;
   }
   if (pid == 0) {
-    printf("I am the child process\n");
+    printf("Child %d:\n". getpid());
+    fflush(stdout);
+    execl(argv[1], argv[1], NULL);
+    perror("execl failed");
+    exit(1);
   } else {
-    printf("I am the parent process\n");
+    wait(NULL);
   }
 
   return 0;
